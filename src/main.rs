4a32466f3e763334f484e0437413e1c194437afc
megatank58 <tanumegatank@gmail.com>
2025-07-07 04:18:36 +0000

    feat: delete button and enclose code in print when provided via string option and show stderr when stdout is empty

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,37 @@ use dotenv_codegen::dotenv;
 use poise::{
     ApplicationContext,
     reply::CreateReply,
-    serenity_prelude::{self as serenity, CreateEmbed, CreateEmbedFooter, CreateAttachment},
+    serenity_prelude::{
+        self as serenity, ButtonStyle, ComponentInteractionCollector, CreateActionRow,
+        CreateAttachment, CreateButton, CreateEmbed, CreateEmbedAuthor, CreateEmbedFooter,
+    },
 };
+
+#[tokio::main]
+async fn main() {
+    let token = dotenv!("DISCORD_TOKEN");
+    let intents = serenity::GatewayIntents::non_privileged();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![calcagebra()],
+
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+
+    client.unwrap().start().await.unwrap();
+}
 
 struct Data {} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
@@ -18,10 +47,12 @@ type Error = Box<dyn std::error::Error + Send + Sync>;
 #[poise::command(slash_command)]
 async fn calcagebra(
     ctx: ApplicationContext<'_, Data, Error>,
-    #[description = "Statement to execute"] code: Option<String>,
+    #[description = "Expression to run [there is no need to enclose it in print()]"] code: Option<
+        String,
+    >,
 ) -> Result<(), Error> {
     let contents = if code.is_some() {
-        code.unwrap()
+        format!("print({})", code.unwrap())
     } else {
         use poise::Modal as _;
 
@@ -44,14 +75,17 @@ async fn calcagebra(
 
     write(&file, &contents).unwrap();
 
-    let response = String::from_utf8(
-        Command::new("./calcagebra.exe")
-            .arg("run")
-            .arg(&file)
-            .output()
-            .unwrap()
-            .stdout,
-    )
+    let result = Command::new("./calcagebra.exe")
+        .arg("run")
+        .arg(&file)
+        .output()
+        .unwrap();
+
+    let response = String::from_utf8(if !result.stdout.is_empty() {
+        result.stdout
+    } else {
+        result.stderr
+    })
     .unwrap();
 
     remove_file(&file).unwrap();
@@ -63,8 +97,17 @@ async fn calcagebra(
         CreateEmbed::new()
             .field("Input", &format!("```rs\n{contents}\n```"), false)
             .field("Output", &format!("```\n{response}\n```"), false)
-            .footer(CreateEmbedFooter::new("Images attached somewhere")),
-    );
+            .footer(CreateEmbedFooter::new("Images attached somewhere"))
+            .author(CreateEmbedAuthor::new(
+                result.status.code().unwrap().to_string(),
+            )),
+    )
+    .components(vec![CreateActionRow::Buttons(vec![
+        CreateButton::new("nonce")
+            .style(ButtonStyle::Danger)
+            .label("Delete")
+            .emoji('🗑'),
+    ])]);
 
     for file in read_dir("./").unwrap().filter(|f| {
         f.as_ref()
@@ -81,29 +124,13 @@ async fn calcagebra(
 
     ctx.send(builder).await?;
 
+    while let Some(press) = ComponentInteractionCollector::new(ctx)
+        .filter(move |press| press.data.custom_id.starts_with("nonce"))
+        .timeout(std::time::Duration::from_secs(60))
+        .await
+    {
+        press.message.delete(ctx).await?;
+    }
+
     Ok(())
-}
-
-#[tokio::main]
-async fn main() {
-    let token = dotenv!("DISCORD_TOKEN");
-    let intents = serenity::GatewayIntents::non_privileged();
-
-    let framework = poise::Framework::builder()
-        .options(poise::FrameworkOptions {
-            commands: vec![calcagebra()],
-            ..Default::default()
-        })
-        .setup(|ctx, _ready, framework| {
-            Box::pin(async move {
-                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
-            })
-        })
-        .build();
-
-    let client = serenity::ClientBuilder::new(token, intents)
-        .framework(framework)
-        .await;
-    client.unwrap().start().await.unwrap();
 }
